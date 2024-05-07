da81301f047f2c640041238a31e63d46c5afb447
Kim Lee <kimberleylee1990@gmail.com>
2024-05-07 10:22:04 +0100

    Create ArticlePage and css component

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,10 +1,13 @@
+import { convertToRelativeDate } from "../utils/dates";
 import { useParams } from "react-router-dom";
-import { useFetch } from "../../useFetch";
+import { useFetch } from "../hooks/useFetch";
 import ErrorMessage from "../components/ErrorMessage";
 import Loading from "../components/Loading";
+import styles from "../css/ArticlePage.module.css";
 
 function ArticlePage() {
   const { article_id } = useParams();
+
   const { data, isLoading, errorMessage } = useFetch({
     path: `/articles/${article_id}`,
   });
@@ -16,9 +19,13 @@ function ArticlePage() {
   return isLoading ? (
     <Loading />
   ) : (
-    <article className="article_page">
+    <article className={styles.ArticlePage}>
       <h1>{data.article.title}</h1>
-      <h2>Author: {data.article.author}</h2>
+      <h2>Written by {data.article.author}</h2>
+      <h3>Topic: {data.article.topic}</h3>
+      <p>Submitted {convertToRelativeDate(data.article.created_at)}</p>
+      <img src={data.article.article_img_url} />
+      <p>{data.article.body}</p>
     </article>
   );
 }
